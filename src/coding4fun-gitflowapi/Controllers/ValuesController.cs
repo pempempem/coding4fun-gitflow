f9b1e5749598cba504ee545ff5d21bbbe6075ac9
Pierre-Etienne Martin <FX10019@adgroup.michelin.com>
2019-01-24 13:39:28 +0100

    TEST1

--- src/coding4fun-gitflowapi/Controllers/ValuesController.cs
+++ src/coding4fun-gitflowapi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace coding4fun_gitflowapi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "152", "value2" };
         }
 
         // GET api/values/5
